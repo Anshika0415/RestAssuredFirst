c979d313939779e23e84d75c37160a1cbacc775b
Anshika <anshikaguptawork11@gmail.com>
2025-10-06 23:55:31 +0530

    commit 3 oct

--- src/test/java/pages/OAuthTestClass.java
+++ src/test/java/pages/OAuthTestClass.java
@@ -3,9 +3,14 @@ package pages;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
+
+import java.util.List;
+
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import pojo.APIClass;
 import pojo.GetCourses;
+import pojo.WebAutomation;
 
 public class OAuthTestClass {
 	
@@ -25,10 +30,30 @@ public class OAuthTestClass {
 		JsonPath js=new JsonPath(tokenResponse);
 		String accessToken=js.getString("access_token");
 		
-		GetCourses response=given().queryParam("access_token", accessToken).when()
+		GetCourses gc=given().queryParam("access_token", accessToken).when()
 				.get("https://rahulshettyacademy.com/oauthapi/getCourseDetails")
-				.then().extract().response().as(GetCourses.class);
-		System.out.println(response.getInstructor());
+				.then().log().all().extract().response().as(GetCourses.class);
+		
+		System.out.println(gc.getInstructor());
+		System.out.println(gc.getCourses().getApi().get(1).getCourseTitle());
+		List<APIClass> apiCourses=gc.getCourses().getApi();
+		for(int i=0;i<apiCourses.size();i++)
+		{
+			if(apiCourses.get(i).getCourseTitle().equalsIgnoreCase("SoapUI Webservices testing"))
+			{
+				System.out.println(apiCourses.get(i).getPrice());
+			}
+		}
+		List<WebAutomation> webCourses=gc.getCourses().getWebAutomation();
+		for(int i=0;i<webCourses.size();i++)
+		{
+			if(webCourses.get(i).getCourseTitle().equalsIgnoreCase("Cypress"))
+			{
+				System.out.println(webCourses.get(i).getCourseTitle());
+				System.out.println(webCourses.get(i).getPrice());
+			}
+		}
+		
 	}
 
 }
